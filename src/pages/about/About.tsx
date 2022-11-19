65a62c7b2f6b6bd6becf9f877cdfef40984093b6
Raul Tormes <raulltormes@gmail.com>
2022-11-19 18:03:10 +0000

    Created a new project

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,18 +1,17 @@
-import { Title } from '../../components/title/Title'
-import { VerticalTitle } from '../../components/verticalTitle/VerticalTitle'
-import './About.scss'
+import { Title } from "../../components/title/Title";
+import { VerticalTitle } from "../../components/verticalTitle/VerticalTitle";
+import "./About.scss";
 
 export const About = () => {
-
-    return (
-        <main id='about'>
-            <div className="title-container">
-            <VerticalTitle name='Who we are?' size={7} />
-            </div>
-            <div className='about-us-content'>
-                <Title name='About us' />
-                <Title name='Mission' type='secondary'/>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main id="about">
+      <div className="title-container">
+        <VerticalTitle name="Who we are?" size={6} />
+      </div>
+      <div className="about-us-content">
+        <Title name="About us" />
+        <Title name="Mission" type="secondary" />
+      </div>
+    </main>
+  );
+};
